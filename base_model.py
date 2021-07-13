e1b1aa18c63f63abebca8197469e3a196127c469
Vladimir Yakovenko <vyakovenko@particle.one>
2021-07-13 03:44:55 +0300

    some working code

--- base_model.py
+++ base_model.py
@@ -7,95 +7,127 @@ import base_model
 import abc
 import collections
 import csv
-from typing import Any, Dict, List
+import datetime as dt
+from typing import List
+
+OUTPUT_FIELD_LIST = [
+    "date",
+    "type",
+    "amount",
+    "from",
+    "to"
+]
 
 
-class AbstractField:
-    """Define an abstract class for a field"""
-
-    def __init__(self, field_name: str, field_type: Any):
-        self.field_name = field_name
-        self.field_type = field_type
-
-    def validate(self, value: Any) -> bool:
-        try:
-            self.field_type(value)
-            return True
-        except Exception:
-            return False
+def bank1_transform_line(values: dict) -> collections.OrderedDict:
+    return collections.OrderedDict([
+        ("date", dt.datetime.strptime(values["timestamp"], "%b %d %Y")),
+        ("type", values["type"]),
+        ("amount", float(values["amount"])),
+        ("from", int(values["from"])),
+        ("to", int(values["to"]))
+    ])
 
 
-class AbstractSchema:
-
-    def __init__(self, schema_name: str, fields: Dict[str, AbstractField]):
-        self.schema_name = schema_name
-        self.fields = fields
-
-    def validate(self, values: dict) -> bool:
-        return all([field.validate(values[name])
-                    for name, field in self.fields.items()])
-
-    @abc.abstractmethod
-    def output(self, values: dict) -> collections.OrderedDict:
-        pass
+def bank2_transform_line(values: dict) -> collections.OrderedDict:
+    return collections.OrderedDict([
+        ("date", dt.datetime.strptime(values["date"], "%d-%m-%Y")),
+        ("type", values["transaction"]),
+        ("amount", float(values["amounts"])),
+        ("from", int(values["from"])),
+        ("to", int(values["to"]))
+    ])
 
 
-class Bank1Schema(AbstractSchema):
+def bank3_transform_line(values: dict) -> collections.OrderedDict:
+    return collections.OrderedDict([
+        (
+            "date",
+            dt.datetime.strptime(values["date_readable"], "%d %b %Y")),
+        ("type", values["type"]),
+        ("amount", float(values["euro"]) + (float(values['cents']) / 10)),
+        ("from", int(values["from"])),
+        ("to", int(values["to"]))
+    ])
 
-    def output(self, values):
-        return collections.OrderedDict([
-            ("name", )
-        ])
 
-
-class AbstractStatementConsumer:
-    def __init__(self, consumer_name: str, schema: AbstractSchema):
-        self.consumer_name = consumer_name
-        self.schema = schema
-
+class AbstractStatementProducer:
     @abc.abstractmethod
     def __iter__(self):
         pass
 
     @abc.abstractmethod
     def __next__(self):
+        pass
+
+
+class CSVStatementProducer(AbstractStatementProducer):
+    def __init__(self,
+                 transform_func,
+                 file_path: str,
+                 delimiter: str = ',',
+                 ):
+        self.file_path = file_path
+        self.delimiter = delimiter
+        self.transform_func = transform_func
+
+    def __iter__(self):
+        csvfile = open(self.file_path)
+        self.reader = csv.DictReader(csvfile, delimiter=self.delimiter)
+        return self
+
+    def __next__(self):
+        return self.transform_func(next(self.reader))
+
+
+class AbstractStatementConsumer(abc.ABC):
+
+    @abc.abstractmethod
+    def write_line(self, line: collections.OrderedDict):
+        pass
+
+    def write_header(self):
         pass
 
 
 class CSVStatementConsumer(AbstractStatementConsumer):
 
     def __init__(self, file_path: str,
-                 delimiter: str = ',',
-                 *args, **kwargs):
-        self.file_path = file_path
-        self.delimiter = delimiter
-        super().__init__(*args, **kwargs)
+                 fieldnames: List[str],
+                 delimiter: str = ','):
+        self.writer = csv.DictWriter(open(file_path, 'w'),
+                                     fieldnames=fieldnames,
+                                     delimiter=delimiter)
 
-    def __iter__(self):
-        return self
+    def write_header(self):
+        self.writer.writeheader()
 
-    def __next__(self):
-        with open(self.file_path) as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=self.delimiter)
-            for line in reader:
-                self.schema.validate(line)
-                return line
-        raise StopIteration
-
-
-class AbstractStatementProducer:
-    def __init__(self, producer_name: str):
-        self.producer_name = producer_name
+    def write_line(self, line):
+        self.writer.writerow(line)
 
 
 class StatementPipeline:
     def __init__(self,
-                 consumers: List[AbstractStatementConsumer],
-                 producers: List[AbstractStatementProducer]):
-        self.consumers = consumers
+                 producers: List[AbstractStatementProducer],
+                 consumer: AbstractStatementConsumer):
         self.producers = producers
+        self.consumer = consumer
 
     def run(self):
-        for consumer in self.consumers:
-            for line in consumer:
-                pass
+        self.consumer.write_header()
+        for producer in self.producers:
+            for line in producer:
+                self.consumer.write_line(line)
+
+
+if __name__ == '__main__':
+
+    bank1 = CSVStatementProducer(bank1_transform_line,
+                                 "data/bank1.csv")
+    bank2 = CSVStatementProducer(bank2_transform_line,
+                                 "data/bank2.csv")
+    bank3 = CSVStatementProducer(bank3_transform_line,
+                                 "data/bank3.csv")
+    main_consumer = CSVStatementConsumer("output1.csv", OUTPUT_FIELD_LIST)
+    pipeline = StatementPipeline([bank1, bank2, bank3], main_consumer)
+    pipeline.run()
